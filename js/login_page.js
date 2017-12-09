4906876ebb64a3041ff0dab4e28645f38dad697c
zcy <584401996@qq.com>
2017-12-09 22:25:37 +0800

    beautify ui

--- js/login_page.js
+++ js/login_page.js
@@ -17,11 +17,11 @@ window.onload = window.onresize =function () {
 
 
         //内容
-        context.font = 'bold 24px arial';
+        context.font = 'bold 24px';
         context.fillStyle = 'black';
         context.fillText('Welcome to EpicMonopoly!', 430, 100);
 
-        context.font = 'bold 18px arial';
+        context.font = 'bold 18px';
         context.fillStyle = 'black';
         context.fillText('Choose your avatar', 650, 170);
 
