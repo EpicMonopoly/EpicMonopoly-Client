6ebe23c833adbd75eb0127e4711040ca88a4b581
Yulian Mao <mintmao@outlook.com>
2018-01-04 01:08:30 +0800

    update

--- static/js/in_game.js
+++ static/js/in_game.js
@@ -2,25 +2,3 @@ function showCfg() {
     
 }
 
-function addToRecords(newEvent) {
-    var recordContent = document.getElementById("recordContent");
-    recordContent.textContent += newEvent +'\r\n';
-    recordContent.scrollTop = recordContent.scrollHeight;
-}
-
-function updateInterest(newInterest) {
-    var statesContent = document.getElementById("stateContent");
-    statesContent.rows[0].cells[0].innerHTML += i;
-}
-
-function setAvatar() {
-    var avatar1 = document.getElementById("avatar1");
-    avatar1.addClass("svg-red");
-    avatar1.src = "static/img/avatar/icon_bug.svg";
-}
-
-function startGame() {
-    var roomID = 0;
-    $.post("/start?roomid="+roomID, function () {
-    });
-}
