54d713673344c3d186b634439b2f3e77367624ba
spacebody <510662916@qq.com>
2018-01-04 06:56:29 +0800

    Add feature

--- static/js/chessboard/Game.js
+++ static/js/chessboard/Game.js
@@ -670,6 +670,11 @@ function WebSocketTest() {
                         button2.visible = false;
                     }
                 }
+
+                if (dat.type == 'record') {
+                    addToRecords(dat.data[0].message);
+                }
+                
             } catch (e) {
                 //not json format
             }
